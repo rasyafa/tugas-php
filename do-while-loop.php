ea8ff1974304f27dae9f50c76bc444b12b51bdcc
Rasya <rasyarasyifah11@gmail.com>
2024-08-20 15:44:21 +0700

    menambahkan file

---
+++ do-while-loop.php
@@ -0,0 +1,25 @@
+//contoh bilangan ganjil menggunakan do while loop
+<?php  
+$i = 1;
+
+do {
+  if ($i % 2 != 0)
+  echo $i . "<br>";
+  $i++;
+} while ($i <= 100) 
+	
+;
+?>  
+
+//contoh bilangan genap menggunakan do while loop
+<?php  
+$i = 1;
+
+do {
+  if ($i % 2 == 0)
+  echo $i . "<br>";
+  $i++;
+} while ($i <= 100) 
+	
+;
+?>  
