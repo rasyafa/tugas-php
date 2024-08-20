d9d9ce375e971198bb8818c45066609280a030eb
Rasya <rasyarasyifah11@gmail.com>
2024-08-20 13:54:06 +0700

    menambahkan file

--- for-loop.php
+++ for-loop.php
@@ -1,7 +1,7 @@
 <?php  
-for ($i = 1; $i <= 100; $i++) {
-	if ($i % 2 == 0) {
-    echo $i . "<br>";
+for ($bilgenap = 1; $bilgenap <= 100; $bilgenap++) {
+	if ($bilgenap % 2 == 0) {
+    echo $bilgenap . "<br>";
     }
 }
 ?>  
---
+++ while-loop.php
@@ -0,0 +1,10 @@
+<?php  
+$bilganjil = 1;
+
+while ($bilganjil <= 100) {
+	if ($bilganjil % 2 != 0) {
+    	echo $bilganjil . "<br>";
+    }
+  	$bilganjil++;
+} 
+?>
